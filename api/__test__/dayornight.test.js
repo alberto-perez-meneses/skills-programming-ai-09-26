09ecbc439d8ee6d68084b54e110456e24349e3b6
alberto-perez-meneses <dr.kidb@gmail.com>
2026-09-12 14:59:40 -0600

    add test step

--- api/__test__/dayornight.test.js
+++ api/__test__/dayornight.test.js
@@ -3,7 +3,7 @@ const { whatPartOfDay } = require('../lib/time');
 describe('whatPartOfDay', () => {
     describe('identifica horario de luz', () => {
         test.each([
-            [25],
+            [25], // outside range
             [12],
             [17]
         ])('retorna Daylight para la hora %s', (hour) => {
